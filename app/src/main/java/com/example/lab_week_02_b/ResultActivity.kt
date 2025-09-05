6c426bdfd422d027990a00a66002e268868b95ae
henrysalim <henrysalim22@gmail.com>
2025-09-05 19:45:34 +0700

    Commit No.5 - Fix ResultActivity to allow resubmitting color code without app restart

--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -20,6 +21,8 @@ class ResultActivity : AppCompatActivity() {
         private const val COLOR_KEY = "COLOR_KEY"
         private const val ERROR_KEY = "ERROR_KEY"
     }
+
+    private val backButton: Button get() = findViewById<Button>(R.id.back_button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +45,15 @@ class ResultActivity : AppCompatActivity() {
 
             val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
             resultMessage.text = getString(R.string.color_code_result_message, colorCode?.uppercase())
+        }
+
+        try {
+            backButton.setOnClickListener {
+                val backIntent = Intent(this, MainActivity::class.java)
+                startActivity(backIntent)
+            }
+        } catch (e: Exception) {
+            Log.e("Error", e.toString())
         }
     }
 }
